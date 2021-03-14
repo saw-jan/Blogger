84e2070cf892631dc829d31b9c7f0547e2c77325
benwade91 <70988204+benn925@users.noreply.github.com>
2021-03-14 16:13:04 -0700

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const dotenv = require('dotenv').config()
@@ -26,5 +26,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/'));
 sequelize.sync({ force: false }).then(() => {
-  app.listen(process.env.PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening'));
 });
