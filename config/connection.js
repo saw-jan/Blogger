34ad03c4126f0b51fa4971efb2bb60a26a0e63d7
benwade91 <70988204+benn925@users.noreply.github.com>
2021-03-14 17:06:31 -0700

    added console.log to connection

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,14 @@ let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
+  console.log(sequelize, 'JAWS_DB true');
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
   });
+  console.log(sequelize, 'LOCAL_DB');
 }
 
 module.exports = sequelize;
